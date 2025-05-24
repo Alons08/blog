ede19a4bfdc7fcff1ed3928b9f213bb23a2f3df7
Your Name <you@example.com>
2025-05-24 15:38:40 -0500

    dc

--- js/script.js
+++ js/script.js
@@ -237,7 +237,7 @@ document.addEventListener("DOMContentLoaded", function () {
         price: 9.0,
         category: "postres",
         image: "images/waffles.jpg",
-        available: false,
+        available: true,
       },
   
       // Bebidas
